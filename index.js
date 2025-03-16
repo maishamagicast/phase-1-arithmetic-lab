07ab535fbffbea6f27f7cdb5ee985f9252c91a29
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-03-16 15:07:34 +0300

    arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+const multiply = num1 * num2; // multiply will be 62
+let random= Math.floor(Math.random()*10)+1;
+let mod = num3 % num4; // mod will be 4
+let max= Math.max(1,20);
